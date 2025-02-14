e06ebcf6917641ee5a8051990a62add16d0bfec7
nelsonrinconc86 <48596015+nelsonrinconc86@users.noreply.github.com>
2025-02-14 07:53:48 -0500

    Add files via upload

--- TiendaEjercicios/src/tiendaejercicios/Ejercicio1.java
+++ TiendaEjercicios/src/tiendaejercicios/Ejercicio1.java
@@ -1,8 +1,25 @@
+/*Pasos a desarrollar:
+1. Crear la Clase 
+2. Crear atributos.
+3. crear metodo constructor
+4. Crear los metodos.
+5. crear el objeto
+6. llamar al objeto
+7. llmar a los metodos
+8. hacer retornos si es necesario
+9. si hay retorno en los metodos hacer almacenamiento
 
+*/
+
+/* observaciones de codigo:
+- Estar estructurado minimo en 3 funciones
+    1. Tomar datos
+    2. Procesar datos
+    3. Mostrar resultados
+*/
 package tiendaejercicios;
 
 //se crea la clase
-
 import java.util.Scanner;
 
 public class Ejercicio1 {
