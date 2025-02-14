6be413c29469f3e13f9df4da965dc51e817e63b3
nelsonrinconc86 <48596015+nelsonrinconc86@users.noreply.github.com>
2025-02-14 07:56:58 -0500

    Update TiendaEjercicios.java

--- TiendaEjercicios/src/tiendaejercicios/TiendaEjercicios.java
+++ TiendaEjercicios/src/tiendaejercicios/TiendaEjercicios.java
@@ -1,3 +1,24 @@
+/*Pasos a desarrollar:
+1. Crear la Clase 
+2. Crear atributos.
+3. crear metodo constructor
+4. Crear los metodos.
+5. crear el objeto
+6. llamar al objeto
+7. llmar a los metodos
+8. hacer retornos si es necesario
+9. si hay retorno en los metodos hacer almacenamiento
+
+*/
+
+/* observaciones de codigo:
+- Estar estructurado minimo en 3 funciones
+    1. Tomar datos
+    2. Procesar datos
+    3. Mostrar resultados
+- el codigo debe tener menu en la clase principal 
+- se debe escoger el ejercicio que se desea ejecutar
+*/
 
 package tiendaejercicios;
 
